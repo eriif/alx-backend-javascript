2c610ff9e1919bd399654dd14a10eeac2259787d
Eric Karanja <erickaranja54@gmail.com>
2024-03-12 23:11:08 +0300

    data manupulation

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,6 @@
 /**
  * Retrieves a list of students.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Eric Karanja <https://github.com/eriif>
  * @returns {{id: Number, firstName: String, location: String}[]}
  */
 export default function getListStudents() {
